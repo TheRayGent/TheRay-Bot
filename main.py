f8f19982c59827b8fbffd58351db3ff45d35a84c
TheRayGent <egortitanpc@gmail.com>
2023-01-31 02:14:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -30,25 +30,42 @@ async def on_member_join(member):
 async def ping(inter):
     await inter.response.send_message("Понг!", ephemeral=True)
 
-@bot.slash_command(name='сообщение', description='Отправка сообщения в любой канал от имени бота')
-async def channel_message(member, *, channel_name=None, message=None):
-    for channel in member.guild.channels:
+@bot.slash_command()
+async def channel_id_mention(ctx,
+    channel_mention: disnake.abc.GuildChannel
+):
+    for channel in ctx.guild.channels:
+        if channel.mention == channel_mention:
+            channel_id = channel.id
+            break
+    channel_send = bot.get_channel(channel_id)
+    await ctx.response.send_message(channel_send)
+
+
+@bot.slash_command(description='Отправка любого сообщения в любой канал данного сервера от имени бота')
+async def message(ctx,
+    text_message: str = commands.Param(name='сообщение', description = 'Напишите текст сообщения'),
+    channel_name = commands.Param(None, name='канал', description = 'Напишите название канала, в который хотите отправить сообщение')
+):
+
+    for channel in ctx.guild.channels:
         if channel.name == channel_name:
             channel_id = channel.id
+            break
 
     if channel_name!=None:
+        
         channel_send = bot.get_channel(channel_id)
 
-        if message==None:
-            await member.response.send_message("Нельзя отправить пустое сообщение!", ephemeral=True)
-        else:
-            await member.response.send_message(f'Сообщение "{message}" отправленно в канал "{channel_send}"', ephemeral=True)
-            await channel_send.send(message)
+        await ctx.response.send_message(f'Сообщение "{text_message}" отправленно в канал "{channel_send}"', ephemeral=True)
+        await channel_send.send(text_message)
+
     elif channel_name==None:
-        if message==None:
-            await member.response.send_message("Нельзя отправить пустое сообщение!", ephemeral=True)
-        else:
-            await member.send(message)
+        
+        channel_send = bot.get_channel(ctx.channel.id)
+        
+        await ctx.response.send_message('Сообщение отправленно!', ephemeral=True)
+        await channel_send.send(text_message)
 
 bot_token = config["bot_token"]
 bot.run(bot_token)
